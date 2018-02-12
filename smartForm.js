62b663b8703ddfd16256219cbf22911f59136963
Badhannn <35439762+Badhannn@users.noreply.github.com>
2018-02-12 09:34:32 +0600

    Add files via upload

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,31 @@
 // Your jQuery goes here
 
 var userData = {};
+name:'',
+email:'',
+html:{Likes:[],Dislikes:[]},
+css::{Likes:[],Dislikes:[]},
+js::{Likes:[],Dislikes:[]},
+strength:{html:'',css:'',js:''},
+currentQuestion:'welcome'
+};
+
+$('start').check(function)(){
+  console.log("start");
+  $('welcome').hide();
+  $(#'q1').show();
+});
+
+
+$('name').change(function(event))
+  console.log($(#name).val());
+});
+$('q1next').click(function)(){
+  if($('name').val()&& $ (email.val()))
+}
+
+
+
+
+
+else('Please enter ur name and email');
