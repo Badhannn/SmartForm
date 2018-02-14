6311235d0077305eac73c2fbaee877f81895c558
Badhannn <35439762+Badhannn@users.noreply.github.com>
2018-02-14 09:26:39 +0600

    Class work 2

--- smartForm.js
+++ smartForm.js
@@ -1,31 +1,61 @@
 // Your jQuery goes here
 
-var userData = {};
-name:'',
-email:'',
-html:{Likes:[],Dislikes:[]},
-css::{Likes:[],Dislikes:[]},
-js::{Likes:[],Dislikes:[]},
-strength:{html:'',css:'',js:''},
-currentQuestion:'welcome'
+var userData = {
+
+name :'',
+email : '',
+html : {likes :[], dislikes:[]},
+css : {likes :[], dislikes:[]} ,
+js : {likes :[], dislikes:[]} ,
+strength: {css: '', js: '', html: ''} ,
+currentQuestion : '#welcome'
+
+
 };
 
-$('start').check(function)(){
-  console.log("start");
-  $('welcome').hide();
-  $(#'q1').show();
-});
+if(localStorage.getItem('userData'))
+{
+  userData= JSON.parse(localStorage.getItem('userData'));
 
-
-$('name').change(function(event))
-  console.log($(#name).val());
-});
-$('q1next').click(function)(){
-  if($('name').val()&& $ (email.val()))
+  $('#welcome').hide();
+  $(userData.currentQuestion).show();
+  $('#name').val(userData.name);
+  $('#email').val(userData.email);
 }
 
+else
+{
+  localStorage.setItem('userData', JSON.stringify(userData));
+}
 
+$('#start').click(function()
+{
+  console.log("start");
+  $('#welcome').hide();
+  $('#q1').show();
+  userData.currentQuestion="#q1";
+  localStorage.setItem('userData' , JSON.stringify(userData));
+});
 
+$('name').change(function(event)
+{
+  //console.log(event);
+  console.log($('#name').val());
+});
 
+$('#next').click(function()
+{
+  if($('#name').val() && $('#email').val())
+  {
+  userData.name= $('name').val();
+  userData.email= $('email').val();
+  $('#welcome').hide();
+  $('#q1').hide();
+  $('#q2').show();
+}
 
-else('Please enter ur name and email');
+else
+{
+  alert('Please Enter Name & email');
+}
+});
